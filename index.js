ee3c8ecc02d1109121bd868a75c023c873e8cdbf
Alex LaFroscia <alex@lafroscia.com>
2018-01-08 17:34:08 -0800

    Add argument parsing through yargs

--- index.js
+++ index.js
@@ -5,28 +5,43 @@ const process = require('process');
 const fs = require('fs');
 
 const Listr = require('listr');
+const yargs = require('yargs');
 
 const Task = require('./lib/task');
 const sortTasks = require('./lib/sort-tasks');
 
-const taskDirectory = process.argv[2] || 'tasks';
-const configDirectory = path.resolve(process.cwd(), taskDirectory);
+function main(taskDirectory) {
+  const configDirectory = path.resolve(process.cwd(), taskDirectory);
 
-const tasks = fs.readdirSync(configDirectory).map(file => {
-  const generateConfig = require(path.join(configDirectory, file));
-  const Klass = generateConfig(Task);
+  const tasks = fs.readdirSync(configDirectory).map(file => {
+    const generateConfig = require(path.join(configDirectory, file));
+    const Klass = generateConfig(Task);
 
-  return new Klass();
-});
-const sortedTasks = sortTasks(tasks);
+    return new Klass();
+  });
+  const sortedTasks = sortTasks(tasks);
 
-const listrTasks = new Listr(
-  sortedTasks.map(task => ({
-    title: task.programName,
-    task: () => {
-      return new Listr(task.toListrTasks());
-    }
-  }))
-);
+  const listrTasks = new Listr(
+    sortedTasks.map(task => ({
+      title: task.programName,
+      task: () => {
+        return new Listr(task.toListrTasks());
+      }
+    }))
+  );
 
-listrTasks.run();
+  return listrTasks.run();
+}
+
+const args = yargs
+  .command('$0 <taskDirectory>', 'run configuration tasks', yargs => {
+    yargs.positional('taskDirectory', {
+      type: 'string',
+      describe: 'path to the task directory'
+    });
+  })
+  .help().argv;
+
+main(args.taskDirectory);
+
+module.exports = main;
