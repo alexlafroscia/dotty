0392f5b413a3f2adde525f68ffee6734f761eba9
Alex LaFroscia <alex@lafroscia.com>
2018-01-09 10:14:39 -0800

    Add update hook to task definition

---
+++ test/task/to-listr/update.test.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+import Task from '../../../lib/task';
+
+test('the `update` task is enabled if the hook is defined', t => {
+  class SomeTask extends Task {
+    update() {}
+  }
+
+  const instance = new SomeTask();
+  t.truthy(instance.toListrTasks()[3].enabled());
+});
+
+test('the `update` task is disabled if the hook is not defined', t => {
+  const instance = new Task();
+  t.falsy(instance.toListrTasks()[3].enabled());
+});
